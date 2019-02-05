033991abcc9219159ccf2f78c8701c270c433537
andechen <chena8@wit.edu>
2019-02-05 16:40:37 -0500

    Added Aesthetic Improvements

--- damoverlay.py
+++ damoverlay.py
@@ -14,11 +14,11 @@ l = float(l)
 l = round(l, 1)
 l = str(l)
 
-unit = 300					#px
+unit = 300								#px
 width = unit * 4
 height = unit * 3
 grid_color = (0, 0, 0)
-line_thickness = 3			#px
+line_thickness = 2						#px
 font = cv.FONT_HERSHEY_PLAIN
 fontsize = 3
 
@@ -40,18 +40,18 @@ cv.line(img, (width, 0), (width, height), grid_color, line_thickness)
 
 xpos = unit * (x - 1)
 ypos = unit * (y - 1)
-xbuff = unit / 5
-ybuff = unit / 2
-textbuff = unit / 4
+xbuff = unit / 5				#buffer to space blue line
+ybuff = unit / 2				#buffer to space blue line
+textbuff = unit / 3				#buffer to space text
 c = (255,0,0)
 
 
 if o == 'h':
-	cv.line(img, (int(xpos + xbuff), int(ypos + ybuff)), (int(xpos + 4 * xbuff), int(ypos + ybuff)), c, line_thickness)
-	cv.putText(img, l, (int(xpos + textbuff), int(ypos + textbuff)), font, fontsize, grid_color, line_thickness)
+	cv.line(img, (int(xpos + xbuff), int(ypos + ybuff)), (int(xpos + 4 * xbuff), int(ypos + ybuff)), c, 5)
+	cv.putText(img, l, (int(xpos + textbuff), int(ypos + textbuff)), font, fontsize, grid_color, 3)
 else:
-	cv.line(img, (int(xpos + ybuff), int(ypos + xbuff)), (int(xpos + ybuff), int(ypos + 4 * xbuff)), c, line_thickness)
-	cv.putText(img,l + ' cm', (int(xpos + textbuff), int(ypos + textbuff)), font, fontsize, grid_color, line_thickness)
+	cv.line(img, (int(xpos + ybuff), int(ypos + xbuff)), (int(xpos + ybuff), int(ypos + 4 * xbuff)), c, 5)
+	cv.putText(img,l + ' cm', (int(xpos + xbuff), int(ypos + unit / 7)), font, fontsize, grid_color, 3)
 
 
 cv.imshow('dam overlay', img)
