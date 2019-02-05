ff393ad2c678a918c9e45f51d4223f72cd98edac
andechen <chena8@wit.edu>
2019-02-04 21:17:39 -0500

    not really updated dam overlay

--- damoverlay.py
+++ damoverlay.py
@@ -2,10 +2,10 @@ import cv2 as cv
 import numpy as np
 import sys
 
-x = sys.argv[1]
-y = sys.argv[2]
-o = sys.argv[3]
-l = sys.argv[4]
+x = sys.argv[1]			#int x position
+y = sys.argv[2]			#int y position
+o = sys.argv[3]			#str orientation
+l = sys.argv[4]			#float crack length
 
 x = int(x)
 y = int(y)
@@ -16,40 +16,35 @@ line_thickness = 3			#px
 font = cv.FONT_HERSHEY_PLAIN
 fontsize = 10
 
-img = np.ones((3 * unit, 4 * unit, 3), np.uint8)
-img[:,0:unit * 4] = (255,255,255)   
+xpos = unit * x
+ypos = unit * y
+xbuff = unit / 5
+ybuff = unit / 2
+c = (255,0,0)
 
-# horizontal grid lines
+img = np.ones((3 * unit, 4 * unit, 3), np.uint8)
+img[:,0:unit * 4] = (255,255,255)
+
 cv.line(img, (0, 0), (4 * unit, 0), grid_color, line_thickness)
 cv.line(img, (0, unit), (4 * unit, unit), grid_color, line_thickness)
 cv.line(img, (0, 2 * unit), (4 * unit, 2 * unit), grid_color, line_thickness)
 cv.line(img, (0, 3 * unit), (4 * unit, 3 * unit), grid_color, line_thickness)
 
-# vertical grid lines
 cv.line(img, (0, 0), (0, 3 * unit), grid_color, line_thickness)
 cv.line(img, (unit, 0), (unit, 3 * unit), grid_color, line_thickness)
 cv.line(img, (2 * unit, 0), (2 * unit, 3 * unit), grid_color, line_thickness)
 cv.line(img, (3 * unit, 0), (3 * unit, 3 * unit), grid_color, line_thickness)
 cv.line(img, (4 * unit, 0), (4 * unit, 3 * unit), grid_color, line_thickness)
 
-line_thickness = int(line_thickness)
 
 if o == 'h':
-	cv.line(img, 
-		(unit * x + unit / 5, unit * y + unit / 2), 
-		(unit * x + 4 * unit / 5, unit * y + unit / 2), 
-		(255, 0, 0), 
-		line_thickness)
+	cv.line(img, (x * unit + unit / 5, y * unit + unit / 2), (x * unit + 4 * unit / 5, y * unit + unit / 2), c, line_thickness)
 else:
-	cv.line(img, 
-		(unit * x + unit / 2, unit * y + unit / 5), 
-		(unit * x + unit / 2, unit * y + 4 * unit / 5), 
-		(255, 0, 0), 
-		line_thickness)
+	cv.line(img, (x * unit + unit / 2, y * unit + unit / 5), (x * unit + unit / 2, y * unit + 4 * unit / 5), c, line_thickness)
+
 	
 l = str(l)
 cv.putText(img, l, (unit * x + unit / 4, unit * y + unit / 4), font, fontsize, grid_color, cv.LINE_AA)
-
 
 cv.imshow('dam overlay', img)
 cv.waitKey(0)
